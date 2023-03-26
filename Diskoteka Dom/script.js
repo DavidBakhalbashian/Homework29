ff0ba284808759c97f73931d482840d842ac4335
David <123312897+DavidBakhalbashian@users.noreply.github.com>
2023-03-26 19:19:27 +0400

    Add files via upload

---
+++ Diskoteka Dom/script.js
@@ -0,0 +1,71 @@
+setInterval(()=>{
+    document.body.style.background  = "red"
+},1000)
+setInterval(()=>{
+    document.body.style.background  = "pink"
+},1300)
+setInterval(()=>{
+    document.body.style.background  = "blue"
+},1500)
+setInterval(()=>{
+    document.body.style.background  = "green"
+},1800)
+//klor1
+setInterval(()=>{
+ document.body.firstElementChild.firstElementChild.style.background = "green"
+},1000)
+setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.style.background= "red"
+   },1300)
+   //klor2
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.style.background= "blue"
+   },1000)
+
+    setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.style.background= "red"
+   },1300)
+   //klor3
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "grey"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "aqua"
+   },1300)
+   //klor4
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "pink"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "grey"
+   },1300)
+   //klor5
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "yellow"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "white"
+   },1300)
+   //klor6
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "Lime"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.style.background= "Teal"
+   },1300)
+   //klor7
+   setInterval(()=>{
+    document.body.firstElementChild.childNodes[13].style.background= "Navy"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.childNodes[13].style.background= "Purple"
+   },1300)
+   //klor8
+   setInterval(()=>{
+    document.body.firstElementChild.childNodes[15].style.background= "DarkRed"
+   },1000)
+   setInterval(()=>{
+    document.body.firstElementChild.childNodes[15].style.background= "DeepPink"
+   },1300)
+  console.log( document.body.firstElementChild.firstElementChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling);
+      
